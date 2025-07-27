12120d09ada9a4b090cc54ffff348c648268ca9f
Kaito Kimura <>
2025-07-27 22:40:27 +0900

    コードを修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import PostResult from "./components/PostResult/PostResult";
 export default async function Home() {
 
   return (
-    <PostResult />
+    <>
+      <h1 className="text-2xl font-bold mb-4 w-full">投稿一覧</h1>
+     <PostResult />
+    </>
+
 
   );
 }
